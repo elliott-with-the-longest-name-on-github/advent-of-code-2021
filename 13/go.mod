4a75d7d38be76a46bb02f45c32f56d53c6c236c1
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2021-12-13 12:35:11 -0700

    feat: Started on day 13, did a good job!

---
+++ 13/go.mod
@@ -0,0 +1,3 @@
+module example.com/aoc13
+
+go 1.17
---
+++ 13/paper/paper_test.go
@@ -0,0 +1,72 @@
+package paper_test
+
+import (
+	"fmt"
+	"testing"
+
+	"example.com/aoc13/paper"
+)
+
+func TestVisibleDotsAfterFold(t *testing.T) {
+	paper := paper.New(
+		map[string]*paper.Dot{
+			"6,10":  {X: 6, Y: 10},
+			"0,14":  {X: 0, Y: 14},
+			"9,10":  {X: 9, Y: 10},
+			"0,3":   {X: 0, Y: 3},
+			"10,4":  {X: 10, Y: 4},
+			"4,11":  {X: 4, Y: 11},
+			"6,0":   {X: 6, Y: 0},
+			"6,12":  {X: 6, Y: 12},
+			"4,1":   {X: 4, Y: 1},
+			"0,13":  {X: 0, Y: 13},
+			"10,12": {X: 10, Y: 12},
+			"3,4":   {X: 3, Y: 4},
+			"3,0":   {X: 3, Y: 0},
+			"8,4":   {X: 8, Y: 4},
+			"1,10":  {X: 1, Y: 10},
+			"2,14":  {X: 2, Y: 14},
+			"8,10":  {X: 8, Y: 10},
+			"9,0":   {X: 9, Y: 0},
+		},
+		0,
+		0,
+	)
+
+	fmt.Println("Paper starting state:")
+	paper.Print()
+	fmt.Print("\n")
+
+	numDotsStart := paper.VisibleDots()
+	if numDotsStart != 18 {
+		t.Errorf("paper reports the wrong number of dots at the start. Wanted %v, got %v", 18, numDotsStart)
+	}
+
+	firstFold, err := paper.Fold("up", 7)
+	if err != nil {
+		t.Errorf("paper failed on the first fold with error %e", err)
+	}
+
+	fmt.Println("Paper state after first fold:")
+	firstFold.Print()
+	fmt.Print("\n")
+
+	numDotsAfterFirstFold := firstFold.VisibleDots()
+	if numDotsAfterFirstFold != 17 {
+		t.Errorf("paper reports the wrong number of dots after the first fold. Wanted %v, got %v", 17, numDotsAfterFirstFold)
+	}
+
+	secondFold, err := firstFold.Fold("left", 5)
+	if err != nil {
+		t.Errorf("paper failed on the second fold with error %e", err)
+	}
+
+	fmt.Println("Paper state after second fold:")
+	secondFold.Print()
+	fmt.Print("\n")
+
+	numDotsAfterSecondFold := secondFold.VisibleDots()
+	if numDotsAfterSecondFold != 16 {
+		t.Errorf("paper reports the wrong number of dots after the second fold. Wanted %v, got %v", 18, numDotsAfterSecondFold)
+	}
+}
